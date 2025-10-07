ee4fb51281c16398a7e9bf9753e1e6703ed04e55
David Lord <lordd007@gmail.com>
2025-10-07 14:43:18 -0700

    Split jobs page into lightweight server and client side Jobs class

--- api/main.py
+++ api/main.py
@@ -12,7 +12,8 @@ _default_origins = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
-    "https://jme-web-staging.herokuapp.com",   # add your web app host here
+    "https://jme-web-staging.herokuapp.com",
+    "https://jme-web-staging-7cea87c089d0.herokuapp.com"  # add your web app host here
 ]
 try:
     _env_origins = json.loads(os.getenv("CORS_ORIGINS", "[]") or "[]")
@@ -23,9 +24,15 @@ try:
 except Exception:
     origins = _default_origins
 
+HEROKU_STAGING_REGEX = r"^https://jme-web-staging(?:-[a-z0-9]+)?\.herokuapp\.com$"
+
+# (optional) read a regex from env to make this configurable later
+CORS_ORIGIN_REGEX = os.getenv("CORS_ORIGIN_REGEX", HEROKU_STAGING_REGEX)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins,               # your exact origins from env/defaults
+    allow_origin_regex=CORS_ORIGIN_REGEX,  # <-- add this line
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
